798e3343017166bbc73e7aaf765e34d0005595b4
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-18 00:06:25 +0000

    importing files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UserController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
+      erb :'/user_views/signup'
+    end
+  end
+  
+  post '/signup' do
+    @user = User.new(params)
+        
+    if @user.save
+      session[:user_id] = @user.id
+      redirect "/users/#{current_user.id}"
+    else
+      redirect '/signup'
+    end
+  end
+  
+  get '/login' do
+    if logged_in?
+      redirect "/users/#{current_user.id}"
+    else
+      erb :'user_views/login'
+    end
+  end
+  
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect "/users/#{@user.id}"
+      else 
+        redirect '/login'
+      end
+    end
+    
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+  
+  get '/users/:id' do
+    if logged_in?
+      @user = current_user
+      @user_goals = Goal.all.select{|goal| goal.user_id == current_user.id}   
+      erb :'/user_views/show'
+    else
+      redirect '/login'
+    end
+  end 
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  validates :username, presence: true, uniqueness: true, length: { in: 2..20}
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { in: 6..15 }
+end
